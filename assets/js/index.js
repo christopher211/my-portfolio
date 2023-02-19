bd0e2df37e7b60ea282a0846784c677c03b28081
Cong Thanh Pham <chrispham211@gmail.com>
2023-02-19 13:26:52 -0600

    Update project info

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+document.querySelector("#main-item").addEventListener("click", function () {
+  window.open("https://project-tracker-app1.herokuapp.com/", "_blank");
+});
+
+document.querySelector("#sub-item-1").addEventListener("click", function () {
+  window.open("https://github.com/zachshouts/music-web-app", "_blank");
+});
